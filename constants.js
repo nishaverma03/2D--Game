252fb220c4cd310d60fe5a6459cbcbe33e8a4875
Nisha Verma <85985602+nishaverma03@users.noreply.github.com>
2026-01-12 11:49:35 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,23 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made some protfolio and learning. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/nishaverma03" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+   I often watch programming videos to upskill and stay updated with new technologies.  
+  You'll also learn how I built this portfolio you're currently playing through!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/nishaverma03" target="_blank"></a>
+  Contact me at hurrynisha03@mail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of my tutorials <a target="_blank">here</a>.`,
+  library: `There are a lot of programming books on my shelves. There is even one in Spanish (I also sometimes learn Spanish btw).
+  I probably only read one of them. `,
+  exit: `If you want to exit Nisha's portfolio, just close the tab.`,
+};
